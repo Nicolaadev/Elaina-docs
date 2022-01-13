0d891dd3618de8a766e214d4c21ab11f65bed010
nk <cainiao1509@gmail.com>
2022-01-13 21:06:38 +0800

    first commit

---
+++ docs/Resources.md
@@ -0,0 +1,25 @@
+---
+sidebar_position: 1
+---
+
+export const Highlight = ({children, color}) => (
+  <span
+    style={{
+      backgroundColor: color,
+      borderRadius: '2px',
+      color: '#fff',
+      padding: '0.2rem',
+    }}>
+    {children}
+  </span>
+);
+
+Elaina Wiki is a website that allows users to find useful tools or content. The website was launched on 8 January 2022, the bot of this project is <span style={{color: 'blue'}}><code>@Nekow#0001</code></span>, which is a Discord developer and web developer. Also, the idea of making this site is by <span style={{color: 'red'}}>`Rydix#1726`</span>
+
+# Note
+
+:::info
+
+**Disclamer:** This is not a official Discord website. Also, we will mention all the information owner.
+
+:::
---
+++ docusaurus.config.js
@@ -0,0 +1,71 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const config = {
+  title: 'Elaina Wiki',
+  tagline: '📦 A bunch of resouces here!',
+  url: 'https://elaina.pages.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/bot1.png',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Elaina Wiki',
+        logo: {
+          alt: 'Elaina logo',
+          src: 'img/bot1.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'Resources',
+            position: 'left',
+            label: 'Resources',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+          {
+            href: 'https://discord.gg/W6FfCHPNGq',
+            label: 'Discord',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Elaina Docs, Inc. Built with Docusaurus. By Nekow`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
